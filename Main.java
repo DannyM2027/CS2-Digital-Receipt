9fc2e11dc65ede0bf619738cc59729a850903714
DannyM2027 <daniel.matut2027@gbwl.org>
2025-10-28 15:08:51 +0000

    DOne

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
 
    public static void main(String []args) {
-
+      Scanner input = new Scanner(System.in);
 
       String highSchoolName = new String("Dtm Highschool");
       double drink = 1.50;
@@ -9,25 +11,38 @@ public class Main {
       double hotDog = 2.75;
       double hamburger = 3.50;
 
+           int orderNumber = (int)(Math.random()*100 + 1);
+
+
+
+      System.out.println("How many drinks do you want?");
+     int nDrinksOrdered = input.nextInt();
+     System.out.println("How many candy do you want?");
+      int nCandiesOrdered = input.nextInt();
+      System.out.println("How many DAWGS do you want?");
+      int nDawgsOrdered = input.nextInt();
+      System.out.println("How many BURGS do you want?");
+      int nBurgsOrdered = input.nextInt();
+      double tax = 0.08;
+      double subTotal = (drink * nDrinksOrdered) + (candy * nCandiesOrdered) + (hotDog * nDawgsOrdered) + (hamburger * nBurgsOrdered);
+      double totalTax = subTotal * tax;
+      double total = totalTax + subTotal;
+
 
 
       // RECEIPT PRINTOUT SECTION
-     System.out.println("**************************************");
-     System.out.println("*                                    *");
-     System.out.println("*     High School Snack Bar          *");
-     System.out.println("*                                    *");
-     System.out.println("*     Drink .........." + drink + "            *");                      
-     System.out.println("*     Candy .........." + candy + "           *");     
-     System.out.println("*     Hot Dog ........" + hotDog + "           *");     
-     System.out.println("*     Hamburger ......" + hamburger + "            *");     
-     System.out.println("*                                    *");    
-     System.out.println("**************************************");
+     System.out.println("********************************************");
+     System.out.println("*          Order Number: " + orderNumber +  "                *");
+     System.out.println("*          High School Snack Bar           *");
+     System.out.println("*                                       6   *");
+     System.out.println("*     Drink .........." + drink + "  Amount: " + nDrinksOrdered + "      *");                      
+     System.out.println("*     Candy .........." + candy + " Amount: " + nCandiesOrdered + "      *");     
+     System.out.println("*     Hot Dog ........" + hotDog + " Amount: " + nDawgsOrdered + "      *");     
+     System.out.println("*     Hamburger ......" + hamburger + "  Amount: " +nBurgsOrdered + "      *");     
+     System.out.println("*             Total:   " + total +   "              *");    
+     System.out.println("*******************************************");
 
 
-     int orderNumber = (int)(Math.random()*100 + 1);
-     int nDrinksOrdered = (int)(Math.random()*3 + 1);
-      int nCandiesOrdered = (int)(Math.random()*3 + 1);
-      int nDawgsOrdered = (int)(Math.random()*3 + 1);
-      int nBurgsOrdered = (int)(Math.random()*3 + 1);
+
    }
 }
